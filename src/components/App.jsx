73bfccf6075d5b103a0e4819bc14f160947fee69
Yriy Ogorodnik <yriyelektric@gmail.com>
2023-07-21 16:33:42 +0300

    mainPage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,9 @@
+import MainPage from 'pages/MainPage/MainPage';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <p>Да начнем мутить грязь )))</p>
+    <div>
+      <MainPage />
     </div>
   );
 };
